55932870b7fa6d379279094039fc8d034077f3d6
Radhakrishan <134207861+Roco75@users.noreply.github.com>
2024-10-15 19:27:40 +0530

    first commit

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -6,6 +6,7 @@ const NotFoundPage = () => {
     <div>
       <h2>404 - Not Found</h2>
       <p>The page you are looking for does not exist.</p>
+      <a href="/">Go to Home</a>
     </div>
   );
 };
